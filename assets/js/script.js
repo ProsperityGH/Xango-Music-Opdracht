29dcfbf750064d264a54de20268d5fa98b741621
TygoVZ <210722@student.glu.nl>
2022-05-23 14:52:02 +0200

    HTML: Added more elements. JS: Progress bar.

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Scroll progress bar
+window.onscroll = function() {progressBar()};
+
+function progressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
