24b59843c1f42821275e95d7d302387de09959f5
TygoVZ <210722@student.glu.nl>
2022-05-30 13:52:47 +0200

    Popup added

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+function tellAFriend() {
+  let parameters = `scrollbars=no, resizable=no, status=no, location=no, toolbar=no, menubar=no, width=700, height=400, left=100, top=100`
+  window.open('https://www.xangomusic.com/mailafriend.php?ref=https%3A//www.xangomusic.com/intro.php', 'Tell a friend - Xango Music', parameters);
+}
